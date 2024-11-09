847dc6efe4894a84e3f74903f6b51454f77adc5e
Kassim Zaid <81573513+kzaid0767@users.noreply.github.com>
2024-11-09 07:54:47 -0500

    app done

--- src/Meme.jsx
+++ src/Meme.jsx
@@ -1,17 +1,71 @@
+import { useEffect, useState } from "react"
+
 export default function Meme(){
+
+    //meme details and url
+    const [meme, setMeme] = useState({toptext:'',bottomtext:'',url:"http://i.imgflip.com/1bij.jpg"})
+
+    //sets text upon user input
+    function handleChange(e){
+        const {name,value} = e.target
+        setMeme(prevState=> ({
+            ...prevState, [name]:value
+        }))
+    }
+
+    //fetch meme image from api
+    useEffect(()=>{
+        fetch('https://api.imgflip.com/get_memes')
+            .then(res=>res.json())
+            .then(data=>{
+                setArrayMemes(data.data.memes)
+            })
+            .catch(()=>{
+                setMeme((prevState)=>({
+                    ...prevState, toptext:'request error', bottomtext:'please try again!'
+                }))
+            })
+    },[])
+
+    //set array of memes
+    const [arrayMemes, setArrayMemes] = useState([])
+
+    //handles form submit to get new meme
+    function handleSubmit(e){
+        e.preventDefault()
+        const idx = Math.floor(Math.random()*arrayMemes.length)
+        setMeme(prevState=>({
+            ...prevState, url:arrayMemes[idx].url
+        }))
+    }
+
     return(
         <main>
-            <form className="appform" action="">
+            <form onSubmit={handleSubmit} className="appform" action="">
                 <input className="input1 inputs" type="text" 
-                    aria-label="top text for the meme" 
-                    name="toptext" 
+                    aria-label="top text for the meme"
+                    placeholder="Meme top text" 
+                    name="toptext"
+                    value={meme.toptext}
+                    onChange={handleChange} 
                     id="toptext" />    
                 <input className="input2 inputs" type="text" 
                     aria-label="bottom text for the meme"
-                    name="bottomtext" 
+                    placeholder="Meme bottom text" 
+                    name="bottomtext"
+                    value={meme.bottomtext} 
+                    onChange={handleChange}
                     id="bottomtext" />
-                <button className="btn">Get Meme</button>    
+                <button className="btn">Get New Meme</button>    
             </form>
+            <div className="imageContainer">
+                <h3 className="toptext text">{meme.toptext}</h3>
+                <img src={meme.url} alt="" />
+                <h3 className="bottomtext text">{meme.bottomtext}</h3>
+            </div>
+
+
+
         </main>
     )
 }
